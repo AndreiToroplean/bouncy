905c2d14567d9370a1d59aea35a1d063b7d55db3
AndreiToroplean <andrei.toroplean@gmail.com>
2020-05-10 12:25:35 +0200

    Implemented the physics without using the pg.Rect class. All the inconsistencies are solved. I guess they were caused by the rounding to int pixel positions. Next step: moving the attributes related to the ball to a new class.

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ BLACK = pg.Color(0, 0, 0)
 GREY = pg.Color(128, 128, 128)
 WHITE = pg.Color(255, 255, 255)
 FPS = 60
-PHYSICS_SUBSTEPS = 2
+PHYSICS_SUBSTEPS = 10
 
 
 class Game:
@@ -21,7 +21,7 @@ class Game:
         self.ball_radius = 25
         self.ball_color = WHITE
 
-        self.ball_rect = pg.draw.circle(self.screen, self.ball_color, self.screen_rect.center, self.ball_radius)
+        self.ball_pos = np.array(self.screen_rect.center, dtype=np.float64)
         self.ball_vel = np.array((0.0, 0.0))
         self.ball_acc = np.array((0.0, 1500 / (FPS*PHYSICS_SUBSTEPS)**2))
         self.ball_restitution = 0.75
@@ -42,17 +42,16 @@ class Game:
             # Logic
             for _ in range(PHYSICS_SUBSTEPS):
                 self.ball_vel += self.ball_acc
-                new_ball_rect = self.ball_rect
-                new_ball_rect.center += self.ball_vel
-                if new_ball_rect.bottom > self.screen_rect.bottom:
+                new_ball_pos = self.ball_pos + self.ball_vel
+                if new_ball_pos[1] + self.ball_radius > self.screen_rect.bottom:
                     self.ball_vel[1] *= -1 * self.ball_restitution
-                    new_ball_rect.bottom -= (new_ball_rect.bottom - self.screen_rect.bottom) * (1 + self.ball_restitution)
-                self.ball_rect = new_ball_rect
+                    new_ball_pos[1] -= (new_ball_pos[1] + self.ball_radius - self.screen_rect.bottom) * (1 + self.ball_restitution)
+                self.ball_pos = new_ball_pos
 
             # Graphics
             self.screen.fill(BLACK)
 
-            self.ball_rect = pg.draw.circle(self.screen, self.ball_color, self.ball_rect.center, self.ball_radius)
+            pg.draw.circle(self.screen, self.ball_color, np.floor(self.ball_pos).astype(np.intc), self.ball_radius)
 
             fps_surf = self.font.render(f"{self.clock.get_fps():.0f}", True, WHITE)
             self.screen.blit(fps_surf, (20, 20))
