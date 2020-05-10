4b3ab8981e824e5fc131ff47b63437724f937432
AndreiToroplean <andrei.toroplean@gmail.com>
2020-05-10 12:07:00 +0200

    This is the best I can do at the moment, using the pg.Rect class. I'm going to try without it.

--- game.py
+++ game.py
@@ -5,22 +5,28 @@ BLACK = pg.Color(0, 0, 0)
 GREY = pg.Color(128, 128, 128)
 WHITE = pg.Color(255, 255, 255)
 FPS = 60
+PHYSICS_SUBSTEPS = 2
 
 
 class Game:
     def __init__(self):
-        self.screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
+        pg.init()
+
+        # self.screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
+        self.screen = pg.display.set_mode((800, 600))
         self.screen_rect = self.screen.get_rect()
         self.clock = pg.time.Clock()
+        self.font = pg.font.SysFont(pg.font.get_default_font(), 12)
 
-        self.pos = np.array(self.screen_rect.center, dtype=np.float64)
-        self.vel = np.array((0.0, 0.0))
-        self.acc = np.array((0.0, 0.2))
+        self.ball_radius = 25
+        self.ball_color = WHITE
 
-        self.radius = 50
+        self.ball_rect = pg.draw.circle(self.screen, self.ball_color, self.screen_rect.center, self.ball_radius)
+        self.ball_vel = np.array((0.0, 0.0))
+        self.ball_acc = np.array((0.0, 1500 / (FPS*PHYSICS_SUBSTEPS)**2))
+        self.ball_restitution = 0.75
 
     def __enter__(self):
-        pg.init()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -30,19 +36,26 @@ class Game:
         while True:
             # Inputs
             for event in pg.event.get():
-                if event.type == pg.KEYDOWN:
-                    if event.key == pg.K_ESCAPE:
-                        return
+                if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE or event.type == pg.QUIT:
+                    return
 
             # Logic
-            self.vel += self.acc
-            self.pos += self.vel
+            for _ in range(PHYSICS_SUBSTEPS):
+                self.ball_vel += self.ball_acc
+                new_ball_rect = self.ball_rect
+                new_ball_rect.center += self.ball_vel
+                if new_ball_rect.bottom > self.screen_rect.bottom:
+                    self.ball_vel[1] *= -1 * self.ball_restitution
+                    new_ball_rect.bottom -= (new_ball_rect.bottom - self.screen_rect.bottom) * (1 + self.ball_restitution)
+                self.ball_rect = new_ball_rect
 
             # Graphics
             self.screen.fill(BLACK)
 
-            ball_rect = pg.draw.circle(self.screen, GREY, self.pos.astype(np.intc), self.radius)
+            self.ball_rect = pg.draw.circle(self.screen, self.ball_color, self.ball_rect.center, self.ball_radius)
 
+            fps_surf = self.font.render(f"{self.clock.get_fps():.0f}", True, WHITE)
+            self.screen.blit(fps_surf, (20, 20))
             pg.display.flip()
 
             # Time
