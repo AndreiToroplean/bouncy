130d77db7c8a5d8bdb547e4408b55e1ae25c24b8
AndreiToroplean <andrei.toroplean@gmail.com>
2020-05-15 12:31:50 +0200

    Moved global_params to their own module.

---
+++ global_params.py
@@ -0,0 +1,10 @@
+import pygame as pg
+
+BLACK = pg.Color(0, 0, 0)
+GREY = pg.Color(128, 128, 128)
+WHITE = pg.Color(255, 255, 255)
+FPS = 120
+PHYSICS_SUBSTEPS = 10
+INPUT_DERIVATIVE = 3
+FULL_SCREEN = False
+DEBUG = False
