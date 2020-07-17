f444b8b1f40df0e016b8e1e012533d77113d7465
AndreiToroplean <andrei.toroplean@gmail.com>
2020-07-17 18:16:50 +0200

    Implemented latency!

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 import random
+from enum import Enum
+from queue import SimpleQueue
 
 import pygame as pg
 import numpy as np
@@ -6,7 +8,7 @@ import numpy as np
 from ball import Ball
 from camera import Camera
 from global_params import FPS, N_PHYSICS_SUBSTEPS, INPUT_DERIVATIVE, DEBUG, \
-    BALL_RADIUS, BOUND_OBST_DIST, BOUND_OBST_WIDTH, BOUND_OBST_HEIGHT, BORDER_S_WIDTH, BALL_ACTION_FORCE
+    BALL_RADIUS, BOUND_OBST_DIST, BOUND_OBST_WIDTH, BOUND_OBST_HEIGHT, BORDER_S_WIDTH, BALL_ACTION_FORCE, LATENCY_FACTOR
 from world import World
 
 
@@ -35,11 +37,17 @@ class Game:
         self._latest_obstacle_w_pos = np.array([0.0, 0.0])
         self._world.spawn_obstacle(np.array([-400.0, 0]), np.array([200.0, 0.0]), self._res)
 
+        self._action_queue = SimpleQueue()
+
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         pg.quit()
+
+    @property
+    def _progress(self):
+        return self._ball.w_pos[0]
 
     def main_loop(self):
         while True:
@@ -51,25 +59,48 @@ class Game:
 
             is_moving_horiz = False
             if keys_pressed[pg.K_LEFT]:
-                self._input_action[0] = -self._input_action_force
+                action_horiz = Action.go_left
                 is_moving_horiz ^= True
             if keys_pressed[pg.K_RIGHT]:
-                self._input_action[0] = self._input_action_force
+                action_horiz = Action.go_right
                 is_moving_horiz ^= True
             if not is_moving_horiz:
-                self._input_action[0] = 0.0
+                action_horiz = Action.stay_horiz
 
             is_moving_vert = False
             if keys_pressed[pg.K_DOWN]:
-                self._input_action[1] = -self._input_action_force
+                action_vert = Action.go_down
                 is_moving_vert ^= True
             if keys_pressed[pg.K_UP]:
-                self._input_action[1] = self._input_action_force
+                action_vert = Action.go_up
                 is_moving_vert ^= True
             if not is_moving_vert:
+                action_vert = Action.stay_vert
+
+            self._action_queue.put((action_horiz, action_vert))
+
+            # Inputs application
+            if self._action_queue.qsize() > self._progress * LATENCY_FACTOR:
+                action_horiz, action_vert = self._action_queue.get()
+            else:
+                action_horiz, action_vert = Action.stay_horiz, Action.stay_vert
+
+            if action_horiz == Action.go_left:
+                self._input_action[0] = -self._input_action_force
+            elif action_horiz == Action.go_right:
+                self._input_action[0] = self._input_action_force
+            else:
+                self._input_action[0] = 0.0
+
+            if action_vert == Action.go_down:
+                self._input_action[1] = -self._input_action_force
+            elif action_vert == Action.go_up:
+                self._input_action[1] = self._input_action_force
+            else:
                 self._input_action[1] = 0.0
 
             # Logic
+            # Obstacles
             if self._camera.w_view[1][0] > self._latest_obstacle_w_pos[0] + BOUND_OBST_DIST[0] - BOUND_OBST_WIDTH[1]/2:
                 height_bound = (self._res[1] * (0.5-BORDER_S_WIDTH) - BALL_RADIUS)
                 w_pos = np.array([
@@ -97,3 +128,13 @@ class Game:
                 self._camera.draw_debug_info()
 
             self._camera.flip_display_and_tick()
+
+
+class Action(Enum):
+    go_right = 0
+    go_left = 1
+    stay_horiz = -1
+
+    go_up = 2
+    go_down = 3
+    stay_vert = -2
