Fixed formula related to ball restitution. Changed enemy progression so it slows now when getting close to the ball. Changed Camera.req_move to take as a parameter a w_pos_x instead of w_pos. Fixed first obstacle collisions. Removed phantom ball in preparation for multiplayer mode. Fixed seed saving. Adjusted SCORE_EXPADD_FACTOR. Tweaked difficulty preset 1.

ball.py
@@ -69,8 +69,7 @@ class Ball:
 
             # r = d−2(d⋅n)n
             w_normal = -w_dir / w_norm
-            self.w_vel = (self.w_vel - 2 * np.dot(self.w_vel, w_normal) * w_normal)
-            self.w_vel *= self._restitution
+            self.w_vel = (self.w_vel - 2 * np.dot(self.w_vel, w_normal) * self._restitution * w_normal)
 
             self.w_pos += (self.radius - w_norm) * (self.w_vel / w_speed) + threshold * w_normal
 

camera.py
@@ -39,8 +39,8 @@ class Camera:
         self._clock.tick(FPS)
         self.time = pg.time.get_ticks()
 
-    def req_move(self, w_pos):
-        self.w_pos += (w_pos - self.w_pos) * (CAM_DAMPING_FACTOR / FPS) * np.array((1, 0))
+    def req_move(self, w_pos_x):
+        self.w_pos[0] += (w_pos_x - self.w_pos[0]) * (CAM_DAMPING_FACTOR / FPS)
 
     def draw(self, obj):
         pix_shift = self._w_pos_to_pix_shift(obj.w_pos)

game.py
@@ -12,7 +12,7 @@ import numpy as np
 from ball import Ball
 from camera import Camera
 from global_params import FPS, N_PHYSICS_SUBSTEPS, DEBUG, BORDER_S_WIDTH, C_DARK_GREY, C_RED, SETTINGS, SEED, SAVES_DIR, \
-    SAVE_PATH, DELAY_BEFORE_QUITTING, LOAD, SAVE, SCORE_EXPADD_FACTOR, SCORE_ADD_FACTOR, DRAW_PHANTOM
+    SAVE_PATH, DELAY_BEFORE_QUITTING, LOAD, SAVE, SCORE_EXPADD_FACTOR, SCORE_ADD_FACTOR
 from rectangle import Rectangle
 from world import World
 
@@ -35,7 +35,6 @@ class Game:
         self._len_der = SETTINGS.INPUT_DERIVATIVE + 1
         self._action_force = SETTINGS.BALL_ACTION_FORCE / (FPS * N_PHYSICS_SUBSTEPS) ** (self._len_der - 1)
         self._action_vec = np.array([0.0, 0.0])
-        self._action_vec_phantom = np.array([0.0, 0.0])
 
         self._action = Action.stay_horiz, Action.stay_vert
 
@@ -51,10 +50,10 @@ class Game:
         self._camera = None
         self._res = None
         self._ball = None
-        self._ball_phantom = None
         self._world = None
         self._latest_obstacle_w_pos = None
         self._enemy = None
+        self._enemy_progress = None
         self._enemy_moving = None
         self._score = None
         self._action_queue = None
@@ -77,16 +76,13 @@ class Game:
             friction=SETTINGS.BALL_FRICTION,
             )
 
-        self._ball_phantom = deepcopy(self._ball)
-        self._ball_phantom.color = C_DARK_GREY
-
         self._world = World(self._res)
 
         self._latest_obstacle_w_pos = np.array([0.0, 0.0])
-        self._world.spawn_obstacle(np.array([-self._res[0] / 4, 0.0]), np.array([-self._res[0] / 8, 0.0]), self._res)
+        self._world.spawn_obstacle(np.array([-self._res[0] / 4, 0.0]), np.array([self._res[0] / 8, 0.0]), self._res)
 
-        self._enemy = Rectangle(*self._camera.w_view, color=C_RED)
-        self._enemy.w_shift = np.array([-self._res[0] * 3 / 4, 0])
+        self._enemy = Rectangle(*(self._camera.w_view - self._res * np.array([0.5, 0.0])), color=C_RED)
+        self._enemy_progress = -self._res[0] * 1 / 4
         self._enemy_moving = False
 
         self._score = 0
@@ -134,20 +130,20 @@ class Game:
 
         action = [Action.stay_horiz, Action.stay_vert]
         is_moving_horiz = False
-        if keys_pressed[pg.K_LEFT]:
+        if keys_pressed[pg.K_LEFT] or keys_pressed[pg.K_a]:
             action[0] = Action.go_left
             is_moving_horiz ^= True
-        if keys_pressed[pg.K_RIGHT]:
+        if keys_pressed[pg.K_RIGHT] or keys_pressed[pg.K_d]:
             action[0] = Action.go_right
             is_moving_horiz ^= True
         if not is_moving_horiz:
             action[0] = Action.stay_horiz
 
         is_moving_vert = False
-        if keys_pressed[pg.K_DOWN]:
+        if keys_pressed[pg.K_DOWN] or keys_pressed[pg.K_s]:
             action[1] = Action.go_down
             is_moving_vert ^= True
-        if keys_pressed[pg.K_UP]:
+        if keys_pressed[pg.K_UP] or keys_pressed[pg.K_w]:
             action[1] = Action.go_up
             is_moving_vert ^= True
         if not is_moving_vert:
@@ -156,20 +152,17 @@ class Game:
         self._action_queue.put(action)
 
         # Inputs application
-        if self._action_queue.qsize() > log(max(1, self._progress * SETTINGS.LATENCY_FACTOR)):
+        latency = log(max(1, self._progress * SETTINGS.LATENCY_FACTOR))
+        while self._action_queue.qsize() > latency:
             self._action = self._action_queue.get()
 
         if self._death_time is None:
             self._action_vec[0] = self._action_map[self._action[0]] * self._action_force
             self._action_vec[1] = self._action_map[self._action[1]] * self._action_force
 
-            self._action_vec_phantom[0] = self._action_map[action[0]] * self._action_force
-            self._action_vec_phantom[1] = self._action_map[action[1]] * self._action_force
-
         else:
             no_action_vec = np.array([0.0, 0.0])
-            self._action_vec = no_action_vec
-            self._action_vec_phantom = no_action_vec
+            self._action_vec[:] = no_action_vec
 
         return True
 
@@ -192,26 +185,24 @@ class Game:
 
         # Balls physics
         self._ball.run_physics(self._action_vec, self._world.colliders)
-        self._ball_phantom.run_physics(self._action_vec_phantom, self._world.colliders)
-
-        if not ((self._camera.w_view[0] < self._ball_phantom.w_pos).all()
-                and (self._ball_phantom.w_pos < self._camera.w_view[1]).all()):
-            for index in range(self._len_der):
-                self._ball_phantom.w_pos_der[index][:] = self._ball.w_pos_der[index]
 
         # Enemy
         if self._death_time is None and self._camera.time > SETTINGS.ENEMY_WAIT:
             self._enemy_moving = True
 
+        enemy_distance = self._ball.radius + self._ball.w_pos[0] - self._enemy_progress
         if self._enemy_moving:
-            self._enemy.w_shift[0] += SETTINGS.ENEMY_SPEED + log(max(1, SETTINGS.ENEMY_LOGADD_SPEED * self._progress))
+            proximity_slowdown = SETTINGS.ENEMY_PROX_COMPENSATION_FACTOR * (1 - 1 / (enemy_distance + SETTINGS.ENEMY_PROX_BIAS + 1) ** SETTINGS.ENEMY_PROX_POW)
+            self._enemy_progress += max(0, (SETTINGS.ENEMY_SPEED + log(max(1, SETTINGS.ENEMY_LOGADD_SPEED * self._progress))) * proximity_slowdown)
+        self._enemy.w_shift[0] = self._enemy_progress
 
-        if self._death_time is None and self._enemy.w_view[1][0] - self._ball.radius > self._ball.w_pos[0]:
+        # Death
+        if self._death_time is None and enemy_distance < 0:
             self._death_time = self._camera.time
             self._enemy_moving = False
 
         # Camera
-        self._camera.req_move(self._ball.w_pos)
+        self._camera.req_move(self._ball.w_pos[0])
 
         # World
         self._world.update_borders(self._camera.w_pos)
@@ -223,8 +214,6 @@ class Game:
     def _draw_graphics(self):
         self._camera.empty_screen()
 
-        if DRAW_PHANTOM:
-            self._camera.draw(self._ball_phantom)
         self._camera.draw(self._ball)
 
         self._camera.draw(self._enemy)
@@ -264,7 +253,7 @@ class Game:
             return
 
         data = {
-            "seed": SEED,
+            "seed": self._seed,
             "difficulty_preset_nb": SETTINGS.DIFFICULTY_PRESET_NB,
             "high_score": self._high_score,
             }